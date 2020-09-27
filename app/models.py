20538be418c330071f4bf8df08b50b11f00e7689
J-Okoto <okotojr@gmail.com>
2020-09-27 18:56:02 +0300

    added a db model class Post

--- app/models.py
+++ app/models.py
@@ -1,10 +1,30 @@
 from . import db
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=True, unique=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
-
+    posts = db.relationship('Post', backref='author', lazy=True)
     def __repr__(self):
         return f'User {self.username}'
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Post title: {self.title}, Date Posted: {self.date_posted}, Post Content: {self.content}'
